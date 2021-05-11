5979d60e6bee327c9db204d3156e093fac53385c
Tyler Saunders <tyler@thesummit.dev>
2021-05-11 08:54:46 -0600

    Add JSON handling to LinkHandler. Add toJSONObject and fromResultSet to Link model.

--- app/src/main/java/dev/thesummit/flink/handlers/LinkHandler.java
+++ app/src/main/java/dev/thesummit/flink/handlers/LinkHandler.java
@@ -3,30 +3,35 @@ package dev.thesummit.flink.handlers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import dev.thesummit.flink.FlinkApplication;
+import dev.thesummit.flink.models.Link;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import org.json.JSONArray;
 
 public class LinkHandler implements HttpHandler {
 
   public void handle(HttpExchange t) throws IOException {
 
-    String response = "";
+    JSONArray arr = new JSONArray();
 
     try {
       Connection conn = FlinkApplication.getContext().pool.getConnection();
       Statement statement = conn.createStatement();
       ResultSet rs = statement.executeQuery("SELECT * from links;");
       while (rs.next()) {
-        response = response + rs.getString("url") + "\n";
+        Link l = Link.fromResultSet(rs);
+        arr.put(l.toJSONObject());
       }
 
     } catch (SQLException e) {
       System.out.println(e);
     }
+
+    String response = arr.toString();
 
     t.sendResponseHeaders(200, response.length());
     OutputStream os = t.getResponseBody();
--- app/src/main/java/dev/thesummit/flink/models/Link.java
+++ app/src/main/java/dev/thesummit/flink/models/Link.java
@@ -8,6 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.UUID;
+import org.json.JSONObject;
 
 public class Link implements DatabaseObject {
 
@@ -21,6 +22,18 @@ public class Link implements DatabaseObject {
   private static String DELETE_QUERY = "DELETE FROM links WHERE id=?;";
   private static String UPDATE_QUERY = "UPDATE links SET url=?, tags=?, unread=? WHERE id=? ";
 
+  /**
+   * Construct a Link instance.
+   *
+   * <p><b>Warning: Does not save the instance to the database. call .add() to commit to database.
+   * </b>
+   *
+   * <p>The id property will remain unset until the instance is added to the database.
+   *
+   * @param url the url.
+   * @param tags the tags.
+   * @param unread whether the link is marked as unread.
+   */
   public Link(String url, String tags, Boolean unread) {
     this.url = url;
     this.tags = tags;
@@ -35,45 +48,14 @@ public class Link implements DatabaseObject {
     return null;
   }
 
-  public UUID getId() {
-    return id;
-  }
-
-  private void setId(UUID id) {
-    this.id = id;
-  }
-
-  public String getUrl() {
-    return url;
-  }
-
-  public void setUrl(String url) {
-    this.url = url;
-  }
-
-  public String getTags() {
-    return tags;
-  }
-
-  public void setTags(String tags) {
-    this.tags = tags;
-  }
-
-  public Boolean getUnread() {
-    return unread;
-  }
-
-  public void setUnread(Boolean unread) {
-    this.unread = unread;
-  }
-
-  /** String representation */
-  @Override
-  public String toString() {
-    return String.format(
-        "Link id %s, with a url of %s and tags of %s", this.id, this.url, this.tags);
-  }
-
+  /**
+   * Adds this link instance to the database.
+   *
+   * <p>Fetches a connection from the application connection pool and attempts to add this link to
+   * the database. Will append the database generated id to this instance when successful.
+   *
+   * @throws SQLException
+   */
   @Override
   public void add() throws SQLException {
     Connection conn = FlinkApplication.getContext().pool.getConnection();
@@ -91,6 +73,15 @@ public class Link implements DatabaseObject {
     FlinkApplication.getContext().pool.releaseConnection(conn);
   }
 
+  /**
+   * Removes this link from the database.
+   *
+   * <p>Fetches a connection from the application connection pool and attempts to remove this link
+   * from the database.
+   *
+   * @param id The id of the link to delete.
+   * @throws SQLException
+   */
   public static void delete(String id) throws SQLException {
     Connection conn = FlinkApplication.getContext().pool.getConnection();
 
@@ -102,6 +93,14 @@ public class Link implements DatabaseObject {
     FlinkApplication.getContext().pool.releaseConnection(conn);
   }
 
+  /**
+   * Updates the corresponding link row in the database to match this instance.
+   *
+   * <p>Fetches a connection from the application connection pool and attempts to update the current
+   * link row in database.
+   *
+   * @throws SQLException
+   */
   @Override
   public void update() throws SQLException {
     Connection conn = FlinkApplication.getContext().pool.getConnection();
@@ -115,5 +114,124 @@ public class Link implements DatabaseObject {
 
     statement.close();
     FlinkApplication.getContext().pool.releaseConnection(conn);
+  }
+
+  /**
+   * Make a human-readable string representaiton of this Link object.
+   *
+   * <p>${id} / ${url} / {$tags}
+   *
+   * @return A printable, displayable, transmittable representation of the Link.
+   */
+  @Override
+  public String toString() {
+    return String.format(
+        "Link id %s, with a url of %s and tags of %s", this.id, this.url, this.tags);
+  }
+
+  /**
+   * Make a JSON compliant object from the link instance.
+   *
+   * @return The JSONObject.
+   */
+  public JSONObject toJSONObject() {
+    JSONObject obj =
+        new JSONObject()
+            .put("id", this.id)
+            .put("url", this.url)
+            .put("tags", this.tags)
+            .put("unread", this.unread);
+
+    return obj;
+  }
+
+  /**
+   * Create a Link instance from a SQL ResultSet row.
+   *
+   * <p>Does not call rs.next() before or after, operates on the current row.
+   *
+   * @param rs A result set row.
+   * @return The link.
+   */
+  public static Link fromResultSet(ResultSet rs) throws SQLException {
+    Link l = new Link(rs.getString("url"), rs.getString("tags"), rs.getBoolean("unread"));
+    l.setId(rs.getObject("id", UUID.class));
+    return l;
+  }
+
+  /**
+   * Getter for the link.id field.
+   *
+   * @return The id value.
+   */
+  public UUID getId() {
+    return id;
+  }
+
+  /**
+   * Internal Setter for the link.id field.
+   *
+   * <p><b> Warning: Must be a UUID </b>
+   *
+   * @param id The id to set.
+   */
+  private void setId(UUID id) {
+    this.id = id;
+  }
+
+  /**
+   * Getter for the link.url field.
+   *
+   * @return The url value.
+   */
+  public String getUrl() {
+    return url;
+  }
+
+  /**
+   * Setter for the link.url field.
+   *
+   * <p><b> Warning: Must be a valid URL </b>
+   *
+   * @param url The url to set.
+   */
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  /**
+   * Getter for the link.tags field.
+   *
+   * @return The tags value.
+   */
+  public String getTags() {
+    return tags;
+  }
+
+  /**
+   * Setter for the link.tags field.
+   *
+   * @param tags The tags to set.
+   */
+  public void setTags(String tags) {
+    this.tags = tags;
+  }
+
+  /**
+   * Getter for the link.unread field.
+   *
+   * @return The unread value.
+   */
+  public Boolean getUnread() {
+    return unread;
+  }
+
+  /**
+   * Setter for the link.unread field.
+   *
+   * @param unread The unread value to set.
+   */
+  public void setUnread(Boolean unread) {
+    this.unread = unread;
   }
 }
