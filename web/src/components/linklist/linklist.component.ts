5ce98f33c81fecb7b57c4c35b0a8e466eb48c9a9
Tyler Saunders <47871226+tylersaunders@users.noreply.github.com>
2021-11-18 19:17:20 -0700

    [Web] Add an empty state to the link list.
    Show empty state when the user is not logged in, or logs out.

--- web/src/components/linklist/linklist.component.ts
+++ web/src/components/linklist/linklist.component.ts
@@ -1,9 +1,10 @@
 import {Component} from '@angular/core';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {switchMap} from 'rxjs/operators';
 import {Link} from 'web/src/models/link';
 import {DataService} from 'web/src/services/data.service';
 import {FilterService} from 'web/src/services/filters.service';
+import {LoginService} from 'web/src/services/login.service';
 
 
 @Component({
@@ -11,15 +12,29 @@ import {FilterService} from 'web/src/services/filters.service';
   templateUrl: './linklist.component.html',
   styleUrls: ['./linklist.component.scss'],
 })
+/** A component that accepts application state and renders a list of links. */
 export class LinkListComponent {
-  links$: Observable<Link[]>;
+  links$: Observable<Link[]|null>;
 
 
   constructor(
       private readonly data: DataService,
       private readonly filters: FilterService,
+      private readonly login: LoginService,
   ) {
-    this.links$ = this.filters.getTagsAsObservable().pipe(
-        switchMap(() => this.data.getLinks()));
+    // Pay attention to the current user status, and if the user is logged in,
+    // immediately fetch the corresponding set of links, given the filters that
+    // are currently set.
+    this.links$ = this.login.getUserAsObservable().pipe(
+        switchMap((user) => {
+          if (user) {
+            return this.filters.getTagsAsObservable().pipe(
+                switchMap(() => this.data.getLinks()));
+          }
+
+          // If there is no user, then display the null state.
+          return of(null);
+        }),
+    );
   }
 }
