a258ef66819943b7b8ccc93790a6e997d910e827
Tyler Saunders <tyler@thesummit.dev>
2022-01-08 19:57:30 -0700

    [WEB] Working settings page with themes!

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import {Component} from '@angular/core';
 import {Observable} from 'rxjs';
+import {User} from 'web/src/models/user';
 import {CookieService} from 'web/src/services/cookie.service';
-
-import {User} from '../models/user';
-import {LoginService} from '../services/login.service';
+import {DataService} from 'web/src/services/data.service';
+import {LoginService} from 'web/src/services/login.service';
+import {UiService} from 'web/src/services/ui.service';
 
 @Component({
   selector: 'app-root',
@@ -13,11 +14,19 @@ import {LoginService} from '../services/login.service';
 /** Base level component */
 export class AppComponent {
   user$: Observable<User|undefined>;
+  readonly defaultTheme = 'light';
 
   constructor(
       private readonly cookie: CookieService,
       private readonly login: LoginService,
+      private readonly ui: UiService,
+      private readonly data: DataService,
   ) {
     this.user$ = this.login.getUserAsObservable();
+
+    // Init page theme
+    this.data.getPreferences().subscribe((prefs) => {
+      this.ui.setPageTheme(prefs.get('theme')?.value || this.defaultTheme);
+    });
   }
 }
--- web/src/views/settings/settings.component.ts
+++ web/src/views/settings/settings.component.ts
@@ -1,7 +1,13 @@
 import {Component} from '@angular/core';
-import {Observable} from 'rxjs';
+import {map, Observable} from 'rxjs';
 import {Preference} from 'web/src/models/preference';
 import {DataService} from 'web/src/services/data.service';
+import {UiService} from 'web/src/services/ui.service';
+
+interface Settings {
+  theme: string;
+  allowNewUsers: boolean;
+}
 
 @Component({
   selector: 'app-settings',
@@ -11,8 +17,44 @@ import {DataService} from 'web/src/services/data.service';
 /** Settings View component */
 export class SettingsViewComponent {
   prefs$: Observable<Map<string, Preference>>;
+  allThemes = this.ui.getAllThemes();
 
-  constructor(private readonly data: DataService) {
-    this.prefs$ = this.data.getPreferences();
+  model: Settings = {
+    theme: 'light',
+    allowNewUsers: true,
+  };
+
+  constructor(
+      private readonly data: DataService,
+      private readonly ui: UiService,
+  ) {
+    this.prefs$ = this.data.getPreferences().pipe(
+        map((prefs) => {
+          // Init form with current settings.
+          if (prefs.has('theme')) {
+            this.model.theme = prefs.get('theme')?.value;
+          }
+          if (prefs.has('allowNewUsers')) {
+            // Parse string boolean to boolean
+            this.model.allowNewUsers =
+                prefs.get('allowNewUsers').value === 'true';
+          }
+          return prefs;
+        }),
+    );
+  }
+
+  /**
+   * Handler for the theme change select.
+   * @param newTheme
+   */
+  onThemeChange(newTheme: string): void {
+    this.ui.setPageTheme(newTheme);
+    this.data.setPreference({key: 'theme', value: newTheme}).subscribe();
+  }
+
+  onAllowNewUsersChange(allowed: boolean) {
+    const value = allowed ? 'true' : 'false';
+    this.data.setPreference({key: 'allowNewUsers', value}).subscribe();
   }
 }
